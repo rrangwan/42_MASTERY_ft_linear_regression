e535b31fce52409ec25395c190416607ed159200
rrangwan <ricky.rangwani@gmail.com>
2024-08-21 22:08:50 +0400

    post submission improvemnts

--- src/B_linear_regression.py
+++ src/B_linear_regression.py
@@ -15,39 +15,14 @@ import pandas as pd
 from A_estimate_price import estimate_price
 
 
-# 1.2 Explore the data
-# Read the data from the csv file
-data = pd.read_csv('../data/data.csv')
-data.dropna(inplace=True)
-mileage = data['km']
-price = data['price']
-
-
-# start with thetas set to 0
-
-with open('../data/theta.csv', mode='w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow([0, 0])
-
-
-
-#Task 2: Implement Linear Regression
-
-#First normalize data to avoid overflow because of large numbers
-min_price = min(price)
-max_price = max(price)
-min_mileage = min(mileage)
-max_mileage = max(mileage)
-
+# Normalize data
 def normalize(data):
     min_val = min(data)
     max_val = max(data)
     return [(x - min_val) / (max_val - min_val) for x in data]
 
-mileage = normalize(mileage)
-price = normalize(price)
 
-#also will later need to denormalize the data
+# also will later need to denormalize the data
 def unnormalized_thetas(theta0, theta1, min_mileage, max_mileage, min_price, max_price):
     unnormalized_theta1 = theta1 * (max_price - min_price) / (max_mileage - min_mileage)
     unnormalized_theta0 = theta0 * (max_price - min_price) + min_price - unnormalized_theta1 * min_mileage
@@ -55,13 +30,13 @@ def unnormalized_thetas(theta0, theta1, min_mileage, max_mileage, min_price, max
 
 
 #This is the regression model (did not use library to do it for me)
-def gradient_descent():
+def gradient_descent(mileage, price, filename):
     m = len(mileage)  # Number of data points
     learning_rate = 0.01 # hyperparameter
     iterations = 10000  # Number of iterations to run gradient descent
 
     #Read theta values from CSV and use for training
-    with open('../data/theta.csv', mode='r') as file:
+    with open(filename, mode='r') as file:
         reader = csv.reader(file)
         theta0, theta1 = next(reader)
         theta0, theta1 = float(theta0), float(theta1)
@@ -83,47 +58,46 @@ def gradient_descent():
         theta1 -= learning_rate * (tmp_theta1 / m)
 
         # Write updated theta values back to CSV in each iteration
-        with open('../data/theta.csv', mode='w', newline='') as file:
+        with open(filename, mode='w', newline='') as file:
             writer = csv.writer(file)
             writer.writerow([theta0, theta1])
 
-
-gradient_descent()
-
+# need to denormalize the data
+def unnormalized_thetas(theta0, theta1, min_mileage, max_mileage, min_price, max_price):
+    unnormalized_theta1 = theta1 * (max_price - min_price) / (max_mileage - min_mileage)
+    unnormalized_theta0 = theta0 * (max_price - min_price) + min_price - unnormalized_theta1 * min_mileage
+    return unnormalized_theta0, unnormalized_theta1
 
 
 # Now to update the theta values as unnormalized
-def unnormalize_thetas(min_mileage, max_mileage, min_price, max_price):
-
+def unnormalize_thetas(min_mileage, max_mileage, min_price, max_price, filename):
     # Read theta values from CSV
-    with open('../data/theta.csv', mode='r') as file:
+    with open(filename, mode='r') as file:
         reader = csv.reader(file)
         theta0, theta1 = next(reader)
         theta0, theta1 = float(theta0), float(theta1)
 
     theta0_n, theta1_n = unnormalized_thetas(theta0, theta1, min_mileage, max_mileage, min_price, max_price)
 
-    with open('../data/theta.csv', mode='w', newline='') as file:
+    with open(filename, mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow([theta0_n, theta1_n])
 
 
-unnormalize_thetas(min_mileage, max_mileage, min_price, max_price)
-
 
 #finally save the predicted price to a csv file called data2.csv
-def create_predicted_prices():
+def create_predicted_prices(filename1, filename2, filename3):
 
     mileage_data = []
     predicted_prices = []
 
     # Read theta values from CSV
-    with open('../data/theta.csv', mode='r') as file:
+    with open(filename2, mode='r') as file:
         reader = csv.reader(file)
         theta0, theta1 = next(reader)
         theta0, theta1 = float(theta0), float(theta1)
 
-    with open('../data/data.csv', mode='r') as file:
+    with open(filename1, mode='r') as file:
         reader = csv.DictReader(file)
         for row in reader:
             mileage = float(row['km'])
@@ -131,11 +105,48 @@ def create_predicted_prices():
             mileage_data.append(mileage)
             predicted_prices.append(predicted_price)
 
-    with open('../data/data2.csv', mode='w', newline='') as file:
+    with open(filename3, mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(['mileage', 'predicted_price'])  # Write the header
         for mileage, predicted_price in zip(mileage_data, predicted_prices):
             writer.writerow([mileage, predicted_price])
 
 
-create_predicted_prices()
+def main():
+    # Explore the data
+    # Read the data from the csv file
+    data = pd.read_csv('../data/data.csv')
+    data.dropna(inplace=True)
+    mileage = data['km']
+    price = data['price']
+    filename1 = '../data/data.csv'
+    filename2 = '../data/theta.csv'
+    filename3 = '../data/data2.csv'
+
+    
+    # start with thetas set to 0
+
+    with open(filename2, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([0, 0])
+
+    #save values before normalizing
+    min_price = min(price)
+    max_price = max(price)
+    min_mileage = min(mileage)
+    max_mileage = max(mileage)
+
+    
+    # Normalize data to avoid overflows (large numbers)
+
+    mileage = normalize(mileage)
+    price = normalize(price)
+
+    gradient_descent(mileage, price, filename2)
+
+    unnormalize_thetas(min_mileage, max_mileage, min_price, max_price, filename2)
+
+    create_predicted_prices(filename1, filename2, filename3)
+
+if __name__ == "__main__":
+    main()
--- src/C_bonus.py
+++ src/C_bonus.py
@@ -5,51 +5,62 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Set the font to Times
-plt.rcParams['font.family'] = 'Times New Roman'
 
-# 1.2 Explore the data
-# Read the data from the csv file
-data = pd.read_csv('../data/data.csv')
-data2 = pd.read_csv('../data/data2.csv')
-
-data.dropna(inplace=True)
-data2.dropna(inplace=True)
-
-mileage = data['km']
-price = data['price']
-predicted_price = data2['predicted_price']
-
-#bonus2: Plot Data
-plt.scatter(mileage, price)
-plt.xlabel('Mileage [in km]')
-plt.ylabel('Price [in USD]')
-plt.title('BONUS_2: Price vs Mileage of Used Cars')
-plt.show()
-
-
-#bonus3: Add Regression line to the plot
-plt.plot(mileage, predicted_price, color="orange", label="Regression Model")
-plt.scatter(x=data['km'], y=data['price'])
-plt.xlabel('Mileage [in km]')
-plt.ylabel('Price [in USD]')
-plt.title('BONUS_3: Price vs Mileage of Used Cars with Regression Line')
-plt.show()
-
-#bonus4: Add Regression line and plot predicted Values
-plt.plot(mileage, predicted_price, color="orange", label="Regression Model")
-plt.scatter(x=data['km'], y=data2['predicted_price'])
-plt.xlabel('Mileage [in km]')
-plt.ylabel('Predicted Price [in USD]')
-plt.title('BONUS-4: Predicted Price vs Mileage of Used Cars with Regression Line')
-plt.show()
-
-
-#bonus5: Mean Absolute Error
 def mean_absolute_error(actual, predicted):
     return sum([abs(a - p) for a, p in zip(actual, predicted)]) / len(actual)
 
-print(f"BONUS_5: The Mean Absolute Error is: {round(mean_absolute_error(price, predicted_price))}\n This is the amount the predicted price is off by (+/-) on average.")
+def main():
+    # Set the font to Times
+    plt.rcParams['font.family'] = 'sans-serif'
+
+    # 1.2 Explore the data
+    # Read the data from the csv file
+    data = pd.read_csv('../data/data.csv')
+    data2 = pd.read_csv('../data/data2.csv')
+
+    data.dropna(inplace=True)
+    data2.dropna(inplace=True)
+
+    mileage = data['km']
+    price = data['price']
+    predicted_price = data2['predicted_price']
+
+    #bonus2: Plot Data
+    plt.scatter(mileage, price)
+    plt.xlabel('Mileage [in km]')
+    plt.ylabel('Price [in USD]')
+    plt.title('BONUS_2: Price vs Mileage of Used Cars')
+    plt.savefig('../plots/plot1.png')
+
+    # may not work outside env
+    #plt.show()
 
 
+    #bonus3: Add Regression line to the plot
+    plt.plot(mileage, predicted_price, color="orange", label="Regression Model")
+    plt.scatter(x=data['km'], y=data['price'])
+    plt.xlabel('Mileage [in km]')
+    plt.ylabel('Price [in USD]')
+    plt.title('BONUS_3: Price vs Mileage of Used Cars with Regression Line')
+    plt.savefig('../plots/plot2.png')
 
+    # may not work outside env
+    #plt.show()
+
+    #bonus4: Add Regression line and plot predicted Values
+    plt.plot(mileage, predicted_price, color="orange", label="Regression Model")
+    plt.scatter(x=data['km'], y=data2['predicted_price'])
+    plt.xlabel('Mileage [in km]')
+    plt.ylabel('Predicted Price [in USD]')
+    plt.title('BONUS-4: Predicted Price vs Mileage of Used Cars with Regression Line')
+    plt.savefig('../plots/plot3.png')
+
+    # may not work outside env
+    #plt.show()
+
+
+    #bonus5: Mean Absolute Error
+    print(f"BONUS_5: The Mean Absolute Error is: {round(mean_absolute_error(price, predicted_price))}\n This is the amount the predicted price is off by (+/-) on average.")
+
+if __name__ == "__main__":
+    main()
